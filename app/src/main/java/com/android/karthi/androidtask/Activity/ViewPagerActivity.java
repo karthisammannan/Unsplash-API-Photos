0c8e7d2be6abcd792244042e003e46eb8a849880
ClearEditText <yourskarthi2@gmail.com>
2018-04-05 17:49:53 +0530

    Bug fixed

--- app/src/main/java/com/android/karthi/androidtask/Activity/ViewPagerActivity.java
+++ app/src/main/java/com/android/karthi/androidtask/Activity/ViewPagerActivity.java
@@ -6,30 +6,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
-import com.android.karthi.androidtask.Fragment.PhotoFragment;
 import com.android.karthi.androidtask.POJO.Result;
 import com.android.karthi.androidtask.R;
 import com.android.karthi.androidtask.Service.DownloadService;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.github.chrisbanes.photoview.OnPhotoTapListener;
+import com.github.chrisbanes.photoview.PhotoView;
+
+import static android.R.attr.uiOptions;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static com.android.karthi.androidtask.Const.Const.My_BROADCAST_ACTION;
 import static com.android.karthi.androidtask.Const.Const.My_INTENT;
-import static com.android.karthi.androidtask.Const.Const.My_INTENT_ARG_POSITION;
 import static com.android.karthi.androidtask.Const.Const.My_INTENT_DOWNLOAD;
 import static com.android.karthi.androidtask.Const.Const.My_INTENT_POSITION;
 import static com.android.karthi.androidtask.Const.Const.My_INTENT_RESPONSE_DOWNLOAD;
@@ -44,11 +50,16 @@ public class ViewPagerActivity extends AppCompatActivity {
     ResponseReceiver responseReceiver;
     NotificationManager mNotifyManager;
     NotificationCompat.Builder mBuilder;
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);  //No title bar is set for the activity
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_view_pager);
+
+
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         icoBack = (ImageView) findViewById(R.id.ico_back);
@@ -79,24 +90,8 @@ public class ViewPagerActivity extends AppCompatActivity {
         });
         itemList = getIntent().getParcelableArrayListExtra(My_INTENT);
         position = getIntent().getIntExtra(My_INTENT_POSITION, 0);
-        initPaging();
-    }
-
-    private void initPaging() {
-        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        System.out.print(itemList.get(0).getUrls().getThumb());
-        for (int i = 0; i < itemList.size(); i++) {
-            PhotoFragment photoFragment = new PhotoFragment();
-            Bundle args = new Bundle();
-            args.putString(My_INTENT_ARG_POSITION, itemList.get(i).getUrls().getSmall());
-            photoFragment.setArguments(args);
-            pagerAdapter.addFragment(photoFragment);
-        }
-
-        viewPager.setAdapter(pagerAdapter);
-        Log.d(TAG, "CurPosition: " + position);
+        viewPager.setAdapter(new SamplePagerAdapter());
         viewPager.setCurrentItem(position);
-
     }
 
     @Override
@@ -115,28 +110,68 @@ public class ViewPagerActivity extends AppCompatActivity {
         localBroadcastManager.unregisterReceiver(responseReceiver);
     }
 
-    public class PagerAdapter extends FragmentPagerAdapter {
-
-        private final List<Fragment> mFragments = new ArrayList<Fragment>();
-
-        public PagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        public void addFragment(Fragment fragment) {
-            mFragments.add(fragment);
-            notifyDataSetChanged();
-        }
-
+    class SamplePagerAdapter extends PagerAdapter {
         @Override
         public int getCount() {
-            return mFragments.size();
+            return itemList.size();
         }
 
         @Override
-        public Fragment getItem(int position) {
-            return mFragments.get(position);
+        public View instantiateItem(ViewGroup container, int position) {
+            PhotoView photoView = new PhotoView(container.getContext());
+            //Glide library to load the image
+            Glide.with(ViewPagerActivity.this)
+                    .load(itemList.get(position).getUrls().getThumb())
+                    .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+                    .into(photoView);
+            // Now just add PhotoView to ViewPager and return it
+            container.addView(photoView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            photoView.setOnPhotoTapListener(new OnPhotoTapListener() {
+                @Override
+                public void onPhotoTap(ImageView view, float x, float y) {
+                    fullScreen();
+                }
+            });
+            return photoView;
         }
+
+        public void fullScreen() {
+            if (flag) {
+                // Hide status bar
+                hideSystemUI();
+                flag = false;
+            } else {
+                // Show status bar
+                showSystemUI();
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                flag = true;
+            }
+        }
+        private void hideSystemUI() {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
+        private void showSystemUI() {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
     }
 
     public class ResponseReceiver extends BroadcastReceiver {
@@ -145,19 +180,19 @@ public class ViewPagerActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             int message = intent.getIntExtra(My_INTENT_RESPONSE_DOWNLOAD, 0);
-            Log.d(TAG, "onReceive: "+message);
+            Log.d(TAG, "onReceive: " + message);
 
-            if(message != 0) {
-                    mBuilder.setProgress(100, message, false);
-                    // Displays the progress bar on notification
-                    mNotifyManager.notify(0, mBuilder.build());
-                } else {
-                    mBuilder.setContentText("Download complete");
-                    // Removes the progress bar
-                    mBuilder.setProgress(0,0,false);
-                    mBuilder.setSmallIcon(android.R.drawable.stat_sys_download_done);
-                    mNotifyManager.notify(0, mBuilder.build());
-                }
+            if (message != 0) {
+                mBuilder.setProgress(100, message, false);
+                // Displays the progress bar on notification
+                mNotifyManager.notify(0, mBuilder.build());
+            } else {
+                mBuilder.setContentText("Download complete");
+                // Removes the progress bar
+                mBuilder.setProgress(0, 0, false);
+                mBuilder.setSmallIcon(android.R.drawable.stat_sys_download_done);
+                mNotifyManager.notify(0, mBuilder.build());
+            }
         }
     }
 }
